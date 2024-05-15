98ceb30afa926e4caef7aee7ecd67cd06575ba9c
Bram <baimz.bramz@gmail.com>
2024-05-15 14:20:44 +0700

    "touchup"

--- app/page.tsx
+++ app/page.tsx
@@ -13,21 +13,24 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p
+          <div
             className={`${lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
+            <strong>Welcome to Acme.</strong> This is the example for the
+            <h1>
+              <strong>MY FIRST DASHBOARD APPLICATIONS</strong>
+            </h1>
+          </div>
+
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+
+          <p>Email: user@nextmail.com</p>
+          <p> Password: 123456</p>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
